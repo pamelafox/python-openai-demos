90fe76b51e26a11df3cd041db995e7675f9835ee
Pamela Fox <pamela.fox@gmail.com>
2025-02-28 16:24:13 -0800

    Adding chained calls example

---
+++ chained_calls.py
@@ -0,0 +1,80 @@
+import os
+
+import azure.identity
+import openai
+from dotenv import load_dotenv
+
+# Setup the OpenAI client to use either Azure, OpenAI.com, or Ollama API
+load_dotenv(override=True)
+API_HOST = os.getenv("API_HOST")
+
+if API_HOST == "azure":
+
+    token_provider = azure.identity.get_bearer_token_provider(
+        azure.identity.DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+    )
+    client = openai.AzureOpenAI(
+        api_version=os.getenv("AZURE_OPENAI_VERSION"),
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        azure_ad_token_provider=token_provider,
+    )
+    MODEL_NAME = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+
+elif API_HOST == "ollama":
+
+    client = openai.OpenAI(
+        base_url=os.getenv("OLLAMA_ENDPOINT"),
+        api_key="nokeyneeded",
+    )
+    MODEL_NAME = os.getenv("OLLAMA_MODEL")
+
+elif API_HOST == "github":
+
+    client = openai.OpenAI(base_url="https://models.inference.ai.azure.com", api_key=os.getenv("GITHUB_TOKEN"))
+    MODEL_NAME = os.getenv("GITHUB_MODEL")
+
+else:
+
+    client = openai.OpenAI(api_key=os.getenv("OPENAI_KEY"))
+    MODEL_NAME = os.getenv("OPENAI_MODEL")
+
+
+response = client.chat.completions.create(
+    model=MODEL_NAME,
+    temperature=0.7,
+    messages=[{"role": "user", "content": "Explain how LLMs work in a single paragraph."}],
+)
+
+explanation = response.choices[0].message.content
+print("Explanation: ", explanation)
+response = client.chat.completions.create(
+    model=MODEL_NAME,
+    temperature=0.7,
+    messages=[
+        {
+            "role": "user",
+            "content": "You're an editor. Review the explanation and provide feedback (but don't edit yourself):\n\n"
+            + explanation,
+        }
+    ],
+)
+
+feedback = response.choices[0].message.content
+print("\n\nFeedback: ", feedback)
+
+response = client.chat.completions.create(
+    model=MODEL_NAME,
+    temperature=0.7,
+    messages=[
+        {
+            "role": "user",
+            "content": (
+                "Revise the article using the following feedback, but keep it to a single paragraph."
+                f"\nExplanation:\n{explanation}\n\nFeedback:\n{feedback}"
+            ),
+        }
+    ],
+)
+
+final_article = response.choices[0].message.content
+print("\n\nFinal Article: ", final_article)
