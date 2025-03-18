f629e181ad20ae3a5c597c6a5df53b0fe3a5def2
Pamela Fox <pamela.fox@gmail.com>
2025-03-18 19:41:00 +0000

    Structured outputs demos

---
+++ structured_outputs_enum.py
@@ -0,0 +1,68 @@
+import os
+from enum import Enum
+
+import azure.identity
+import openai
+import rich
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+# Setup the OpenAI client to use either Azure, OpenAI.com, or Ollama API
+load_dotenv(override=True)
+API_HOST = os.getenv("API_HOST", "github")
+
+if API_HOST == "azure":
+    token_provider = azure.identity.get_bearer_token_provider(
+        azure.identity.DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+    )
+    client = openai.AzureOpenAI(
+        api_version=os.environ["AZURE_OPENAI_VERSION"],
+        azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+        azure_ad_token_provider=token_provider,
+    )
+    MODEL_NAME = os.environ["AZURE_OPENAI_DEPLOYMENT"]
+
+elif API_HOST == "ollama":
+    client = openai.OpenAI(base_url=os.environ["OLLAMA_ENDPOINT"], api_key="nokeyneeded")
+    MODEL_NAME = os.environ["OLLAMA_MODEL"]
+
+elif API_HOST == "github":
+    client = openai.OpenAI(base_url="https://models.inference.ai.azure.com", api_key=os.environ["GITHUB_TOKEN"])
+    MODEL_NAME = os.getenv("GITHUB_MODEL", "gpt-4o")
+
+else:
+    client = openai.OpenAI(api_key=os.environ["OPENAI_KEY"])
+    MODEL_NAME = os.environ["OPENAI_MODEL"]
+
+
+
+class DayOfWeek(str, Enum):
+  SUNDAY = "Sunday"
+  MONDAY = "Monday"
+  TUESDAY = "Tuesday"
+  WEDNESDAY = "Wednesday"
+  THURSDAY = "Thursday"
+  FRIDAY = "Friday"
+  SATURDAY = "Saturday"
+
+class CalendarEvent(BaseModel):
+  name: str
+  date: DayOfWeek
+  participants: list[str]
+
+completion = client.beta.chat.completions.parse(
+    model=MODEL_NAME,
+    messages=[
+        {"role": "system", "content": "Extract the event information."},
+        {"role": "user", "content": "Alice and Bob are going to a science fair on Friday."},
+    ],
+    response_format=CalendarEvent,
+)
+
+
+message = completion.choices[0].message
+if (message.refusal):
+    rich.print(message.refusal)
+else:
+    event = message.parsed
+    rich.print(event)
---
+++ structured_outputs_function_calling.py
@@ -0,0 +1,49 @@
+import os
+
+import azure.identity
+import openai
+import rich
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+# Setup the OpenAI client to use either Azure, OpenAI.com, or Ollama API
+load_dotenv(override=True)
+API_HOST = os.getenv("API_HOST", "github")
+
+if API_HOST == "azure":
+    token_provider = azure.identity.get_bearer_token_provider(
+        azure.identity.DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+    )
+    client = openai.AzureOpenAI(
+        api_version=os.environ["AZURE_OPENAI_VERSION"],
+        azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+        azure_ad_token_provider=token_provider,
+    )
+    MODEL_NAME = os.environ["AZURE_OPENAI_DEPLOYMENT"]
+
+elif API_HOST == "ollama":
+    client = openai.OpenAI(base_url=os.environ["OLLAMA_ENDPOINT"], api_key="nokeyneeded")
+    MODEL_NAME = os.environ["OLLAMA_MODEL"]
+
+elif API_HOST == "github":
+    client = openai.OpenAI(base_url="https://models.inference.ai.azure.com", api_key=os.environ["GITHUB_TOKEN"])
+    MODEL_NAME = os.getenv("GITHUB_MODEL", "gpt-4o")
+
+else:
+    client = openai.OpenAI(api_key=os.environ["OPENAI_KEY"])
+    MODEL_NAME = os.environ["OPENAI_MODEL"]
+
+
+class GetDeliveryDate(BaseModel):
+  order_id: str
+
+response = client.chat.completions.create(
+   model=MODEL_NAME,
+   messages=[
+     {"role": "system",
+      "content": "You're a customer support bot. Use the tools to assist the user."},
+     {"role": "user",
+      "content": "Hi, can you tell me the delivery date for my order #12345?"}],
+   tools=[openai.pydantic_function_tool(GetDeliveryDate)])
+
+rich.print(response.choices[0].message.tool_calls[0].function)
